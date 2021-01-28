5556b7e96f6e9ad7a772cc667efd4a8bc51adc1f
stshenzhaoliang <409178030@qq.com>
2021-01-28 16:28:31 +0800

    day06-servlet

--- day06/src/main/java/cn/st/servlet/MyServlet.java
+++ day06/src/main/java/cn/st/servlet/MyServlet.java
@@ -17,10 +17,12 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("-----------");
+        req.getRequestDispatcher("/hello.jsp").forward(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("-----------");
+        this.doGet(req, resp);
     }
 }
